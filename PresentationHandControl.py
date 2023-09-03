7924bb251dbd2f3dc1b5fcf3379bb67d6147c876
AnonymousAAArdvark <andrewhuiyang0830@gmail.com>
2023-09-03 00:39:10 -0700

    added key control for gesture control

--- PresentationHandControl.py
+++ PresentationHandControl.py
@@ -13,11 +13,13 @@ cap.set(3, wCam)
 cap.set(4, hCam)
 pTime = 0
 pointingGesture = []
-detect_repeat = 0
+prevAction = "None"
+nRepeats = 0
 detector = htm.HandDetector(detectionCon=0.8)
 
 while True:
     success, img = cap.read()
+    img = cv2.flip(img, 1)
     img = detector.findHands(img)
     lmList = detector.findPosition(img, draw=False)
     pointingGesture = []
@@ -35,11 +37,34 @@ while True:
     cv2.putText(img, f'Orientation: {pointingGesture}', (20, 100), cv2.FONT_HERSHEY_SIMPLEX,
                 .7, (255, 0, 0), 2)
 
+    action = "None"
+    vote = 0
     for idx, hand in enumerate(pointingGesture):
-        if hand != "None":
-            detect_repeat += 1
-        else:
-            detect_repeat = 0
+        if hand == "Left":
+            vote -= 1
+        elif hand == "Right":
+            vote += 1
+
+    if vote > 0:
+        action = "Right"
+    elif vote < 0:
+        action = "Left"
+    else:
+        action = "None"
+
+    if action == prevAction and prevAction != "None":
+        nRepeats += 1
+    else:
+        prevAction = action
+        nRepeats = 0
+
+    if nRepeats == 2:
+        kbf.arrowKeyPress(action)
+        print("pressed")
+    elif nRepeats % 6 == 0 and nRepeats != 0:
+        kbf.arrowKeyPress(action)
+        print("pressed")
+
     detector.drawHands(img, pointingGesture)
 
     cv2.imshow("Img", img)
