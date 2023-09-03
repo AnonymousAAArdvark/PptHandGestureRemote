2cfbe9e2eb5ef6a69b1503203a43d23712995c5f
AnonymousAAArdvark <andrewhuiyang0830@gmail.com>
2023-09-02 23:04:12 -0700

    added multi hand color draw

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -4,7 +4,7 @@ import time
 
 
 class HandDetector:
-    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+    def __init__(self, mode=False, maxHands=10, detectionCon=0.5, trackCon=0.5):
         self.mode = mode
         self.maxHands = maxHands
         self.detectionCon = detectionCon
@@ -16,32 +16,40 @@ class HandDetector:
                                         min_tracking_confidence=self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
 
-    def findHands(self, img, draw=True, color=(255, 0, 0)):
+    def findHands(self, img):
         imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
-        if self.results.multi_hand_landmarks:
-            for handLms in self.results.multi_hand_landmarks:
-                if draw:
-                    self.mpDraw.draw_landmarks(img, handLms, self.mphands.HAND_CONNECTIONS,
-                                               self.mpDraw.DrawingSpec(color))
         return img
 
-    def findPosition(self, img, handNum=0, draw=True):
+    def findPosition(self, img, draw=True):
         lmList = []
         if self.results.multi_hand_landmarks:
-            myHand = self.results.multi_hand_landmarks[handNum]
-            for id, lm in enumerate(myHand.landmark):
-                # print(id, lm)
-                h, w, c = img.shape
-                cx, cy = int(lm.x * w), int(lm.y * h)
-                # print(id, cx, cy)
-                lmList.append([id, cx, cy, lm.x, lm.y, lm.z])
-                if draw:
-                    cv.circle(img, (cx, cy), 15, (255, 0, 255), cv.FILLED)
+            for myHand in self.results.multi_hand_landmarks:
+                hand = []
+                for id, lm in enumerate(myHand.landmark):
+                    h, w, c = img.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    hand.append([id, cx, cy, lm.x, lm.y, lm.z])
+                    if draw:
+                        cv.circle(img, (cx, cy), 15, (255, 0, 255), cv.FILLED)
+                lmList.append(hand)
 
         return lmList
 
-
+    def drawHands(self, img, pointingGesture):
+        if self.results.multi_hand_landmarks:
+            for idx, handLms in enumerate(self.results.multi_hand_landmarks):
+                if len(pointingGesture) > idx:
+                    if pointingGesture[idx] == "Left":
+                        handColor = (0, 255, 255)
+                    elif pointingGesture[idx] == "Right":
+                        handColor = (0, 255, 0)
+                    else:
+                        handColor = (255, 0, 0)
+                else:
+                    handColor = (255, 0, 0)
+                self.mpDraw.draw_landmarks(img, handLms, self.mphands.HAND_CONNECTIONS,
+                                           self.mpDraw.DrawingSpec(handColor))
 
 
 def main():
