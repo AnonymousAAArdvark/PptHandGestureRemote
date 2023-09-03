63b6d4ad15cbcf7d1c9b203fba1075e355374193
AnonymousAAArdvark <andrewhuiyang0830@gmail.com>
2023-09-03 00:38:53 -0700

    simplified keypress

--- KeyboardFunctions.py
+++ KeyboardFunctions.py
@@ -2,10 +2,6 @@ import cv2
 import mediapipe as mp
 import time
 import pyautogui
-#pip install pyautogui
-def leftArrowPress():
-    pyautogui.press('left')
-def rightArrowPress():
-    pyautogui.press('right')
 
-
+def arrowKeyPress(direction):
+    pyautogui.press(direction.lower())
